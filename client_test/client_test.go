324cd465df2b10beb8cea37647ec9629abb77861
jeremyquek <jeremyqjs@gmail.com>
2025-10-21 20:29:56 -0700

    More tests added

--- client_test/client_test.go
+++ client_test/client_test.go
@@ -9,6 +9,7 @@ import (
 	_ "encoding/hex"
 	_ "errors"
 	_ "strconv"
+	"strings"
 	_ "strings"
 	"testing"
 
@@ -20,9 +21,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	userlib "github.com/cs161-staff/project2-userlib"
+	"fmt"
 
 	"github.com/cs161-staff/project2-starter-code/client"
+	userlib "github.com/cs161-staff/project2-userlib"
 )
 
 func TestSetupAndExecution(t *testing.T) {
@@ -250,112 +252,380 @@ var _ = Describe("Client Tests", func() {
 		})
 
 		// User-auth
+		Describe("User Authentication Testing", func() {
+			Specify("Case-sensitive usernames", func() {
+				alice, err = client.InitUser("alice", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// Specify("Case-sensitive usernames", func() {
-		// 	alice, err = client.InitUser("alice", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				aliceUpper, err := client.InitUser("Alice", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// 	aliceUpper, err := client.InitUser("Alice", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				// Both should be different users
+				err = alice.StoreFile(aliceFile, []byte(contentOne))
+				Expect(err).To(BeNil())
 
-		// 	// Both should be different users
-		// 	err = alice.StoreFile(aliceFile, []byte(contentOne))
-		// 	Expect(err).To(BeNil())
+				err = aliceUpper.StoreFile(aliceFile, []byte(contentTwo))
+				Expect(err).To(BeNil())
 
-		// 	err = aliceUpper.StoreFile(aliceFile, []byte(contentTwo))
-		// 	Expect(err).To(BeNil())
+				data, err := alice.LoadFile(aliceFile)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(contentOne)))
 
-		// 	data, err := alice.LoadFile(aliceFile)
-		// 	Expect(err).To(BeNil())
-		// 	Expect(data).To(Equal([]byte(contentOne)))
+				data, err = aliceUpper.LoadFile(aliceFile)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(contentTwo)))
+			})
 
-		// 	data, err = aliceUpper.LoadFile(aliceFile)
-		// 	Expect(err).To(BeNil())
-		// 	Expect(data).To(Equal([]byte(contentTwo)))
-		// })
+			Specify("Special characters in username", func() {
+				_, err = client.InitUser("alice@email.com", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// Specify("Special characters in username", func() {
-		// 	_, err = client.InitUser("alice@email.com", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				_, err = client.InitUser("user-name_123", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// 	_, err = client.InitUser("user-name_123", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				_, err = client.InitUser("user with spaces", defaultPassword)
+				Expect(err).To(BeNil())
+			})
 
-		// 	_, err = client.InitUser("user with spaces", defaultPassword)
-		// 	Expect(err).To(BeNil())
-		// })
+			Specify("Empty password is valid", func() {
+				alice, err = client.InitUser("alice", "")
+				Expect(err).To(BeNil())
 
-		// Specify("Empty password is valid", func() {
-		// 	alice, err = client.InitUser("alice", "")
-		// 	Expect(err).To(BeNil())
+				aliceLaptop, err = client.GetUser("alice", "")
+				Expect(err).To(BeNil())
 
-		// 	aliceLaptop, err = client.GetUser("alice", "")
-		// 	Expect(err).To(BeNil())
+				err = alice.StoreFile(aliceFile, []byte(contentOne))
+				Expect(err).To(BeNil())
 
-		// 	err = alice.StoreFile(aliceFile, []byte(contentOne))
-		// 	Expect(err).To(BeNil())
+				data, err := aliceLaptop.LoadFile(aliceFile)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(contentOne)))
+			})
 
-		// 	data, err := aliceLaptop.LoadFile(aliceFile)
-		// 	Expect(err).To(BeNil())
-		// 	Expect(data).To(Equal([]byte(contentOne)))
-		// })
+			Specify("Long username and password", func() {
+				longUsername := strings.Repeat("a", 1000)
+				longPassword := strings.Repeat("p", 1000)
 
-		// Specify("Long username and password", func() {
-		// 	longUsername := strings.Repeat("a", 1000)
-		// 	longPassword := strings.Repeat("p", 1000)
+				alice, err = client.InitUser(longUsername, longPassword)
+				Expect(err).To(BeNil())
 
-		// 	alice, err = client.InitUser(longUsername, longPassword)
-		// 	Expect(err).To(BeNil())
+				aliceLaptop, err = client.GetUser(longUsername, longPassword)
+				Expect(err).To(BeNil())
+			})
 
-		// 	aliceLaptop, err = client.GetUser(longUsername, longPassword)
-		// 	Expect(err).To(BeNil())
-		// })
+			Specify("Password change should fail (implicit test)", func() {
+				alice, err = client.InitUser("alice", "password1")
+				Expect(err).To(BeNil())
 
-		// Specify("Password change should fail (implicit test)", func() {
-		// 	alice, err = client.InitUser("alice", "password1")
-		// 	Expect(err).To(BeNil())
+				// Try to "change" password by creating user again
+				_, err = client.InitUser("alice", "password2")
+				Expect(err).ToNot(BeNil())
 
-		// 	// Try to "change" password by creating user again
-		// 	_, err = client.InitUser("alice", "password2")
-		// 	Expect(err).ToNot(BeNil())
+				// Original password should still work
+				aliceLaptop, err = client.GetUser("alice", "password1")
+				Expect(err).To(BeNil())
 
-		// 	// Original password should still work
-		// 	aliceLaptop, err = client.GetUser("alice", "password1")
-		// 	Expect(err).To(BeNil())
+				// New password should not work
+				_, err = client.GetUser("alice", "password2")
+				Expect(err).ToNot(BeNil())
+			})
 
-		// 	// New password should not work
-		// 	_, err = client.GetUser("alice", "password2")
-		// 	Expect(err).ToNot(BeNil())
-		// })
+			Specify("Multiple concurrent sessions for same user", func() {
+				alice, err = client.InitUser("alice", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// Specify("Multiple concurrent sessions for same user", func() {
-		// 	alice, err = client.InitUser("alice", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				aliceLaptop, err = client.GetUser("alice", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// 	aliceLaptop, err = client.GetUser("alice", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				alicePhone, err = client.GetUser("alice", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// 	alicePhone, err = client.GetUser("alice", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				aliceDesktop, err = client.GetUser("alice", defaultPassword)
+				Expect(err).To(BeNil())
 
-		// 	aliceDesktop, err = client.GetUser("alice", defaultPassword)
-		// 	Expect(err).To(BeNil())
+				// All sessions should be independent but see same data
+				err = alice.StoreFile(aliceFile, []byte(contentOne))
+				Expect(err).To(BeNil())
 
-		// 	// All sessions should be independent but see same data
-		// 	err = alice.StoreFile(aliceFile, []byte(contentOne))
-		// 	Expect(err).To(BeNil())
+				data, err := aliceLaptop.LoadFile(aliceFile)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(contentOne)))
 
-		// 	data, err := aliceLaptop.LoadFile(aliceFile)
-		// 	Expect(err).To(BeNil())
-		// 	Expect(data).To(Equal([]byte(contentOne)))
+				data, err = alicePhone.LoadFile(aliceFile)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(contentOne)))
 
-		// 	data, err = alicePhone.LoadFile(aliceFile)
-		// 	Expect(err).To(BeNil())
-		// 	Expect(data).To(Equal([]byte(contentOne)))
+				data, err = aliceDesktop.LoadFile(aliceFile)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(contentOne)))
+			})
+		})
+	})
 
-		// 	data, err = aliceDesktop.LoadFile(aliceFile)
-		// 	Expect(err).To(BeNil())
-		// 	Expect(data).To(Equal([]byte(contentOne)))
-		// })
+	Describe("File Operations Testing", func() {
+		Specify("Empty filename is valid", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			err = alice.StoreFile("", []byte(contentOne))
+			Expect(err).To(BeNil())
+
+			data, err := alice.LoadFile("")
+			Expect(err).To(BeNil())
+			Expect(data).To(Equal([]byte(contentOne)))
+		})
+
+		Specify("Special characters in filename", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			specialNames := []string{
+				"file with spaces.txt",
+				"file@#$%.txt",
+				"file/with/slashes.txt",
+				"file\\with\\backslashes.txt",
+				"file\nwith\nnewlines.txt",
+				"file\twith\ttabs.txt",
+			}
+
+			for _, name := range specialNames {
+				err = alice.StoreFile(name, []byte(contentOne))
+				Expect(err).To(BeNil())
+
+				data, err := alice.LoadFile(name)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(contentOne)))
+			}
+		})
+
+		Specify("Very long filename", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			longFilename := strings.Repeat("a", 10000)
+
+			err = alice.StoreFile(longFilename, []byte(contentOne))
+			Expect(err).To(BeNil())
+
+			data, err := alice.LoadFile(longFilename)
+			Expect(err).To(BeNil())
+			Expect(data).To(Equal([]byte(contentOne)))
+		})
+
+		Specify("Binary file content", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			binaryContent := make([]byte, 1000)
+			for i := range binaryContent {
+				binaryContent[i] = byte(i % 256)
+			}
+
+			err = alice.StoreFile(aliceFile, binaryContent)
+			Expect(err).To(BeNil())
+
+			data, err := alice.LoadFile(aliceFile)
+			Expect(err).To(BeNil())
+			Expect(data).To(Equal(binaryContent))
+		})
+
+		Specify("File with null bytes", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			contentWithNulls := []byte("content\x00with\x00null\x00bytes")
+
+			err = alice.StoreFile(aliceFile, contentWithNulls)
+			Expect(err).To(BeNil())
+
+			data, err := alice.LoadFile(aliceFile)
+			Expect(err).To(BeNil())
+			Expect(data).To(Equal(contentWithNulls))
+		})
+
+		Specify("StoreFile completely overwrites including previous appends", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			err = alice.StoreFile(aliceFile, []byte(contentOne))
+			Expect(err).To(BeNil())
+
+			err = alice.AppendToFile(aliceFile, []byte(contentTwo))
+			Expect(err).To(BeNil())
+
+			err = alice.AppendToFile(aliceFile, []byte(contentThree))
+			Expect(err).To(BeNil())
+
+			// Overwrite entire file
+			err = alice.StoreFile(aliceFile, []byte("new content"))
+			Expect(err).To(BeNil())
+
+			data, err := alice.LoadFile(aliceFile)
+			Expect(err).To(BeNil())
+			Expect(data).To(Equal([]byte("new content")))
+		})
+
+		Specify("Multiple users, multiple files each", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			bob, err = client.InitUser("bob", defaultPassword)
+			Expect(err).To(BeNil())
+
+			// Alice creates multiple files
+			for i := 0; i < 10; i++ {
+				filename := fmt.Sprintf("alice_file_%d.txt", i)
+				err = alice.StoreFile(filename, []byte(fmt.Sprintf("alice content %d", i)))
+				Expect(err).To(BeNil())
+			}
+
+			// Bob creates multiple files
+			for i := 0; i < 10; i++ {
+				filename := fmt.Sprintf("bob_file_%d.txt", i)
+				err = bob.StoreFile(filename, []byte(fmt.Sprintf("bob content %d", i)))
+				Expect(err).To(BeNil())
+			}
+
+			// Verify all files
+			for i := 0; i < 10; i++ {
+				filename := fmt.Sprintf("alice_file_%d.txt", i)
+				data, err := alice.LoadFile(filename)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(fmt.Sprintf("alice content %d", i))))
+			}
+
+			for i := 0; i < 10; i++ {
+				filename := fmt.Sprintf("bob_file_%d.txt", i)
+				data, err := bob.LoadFile(filename)
+				Expect(err).To(BeNil())
+				Expect(data).To(Equal([]byte(fmt.Sprintf("bob content %d", i))))
+			}
+		})
+	})
+
+	Describe("Sharing/Revocation Testing", func() {
+		Specify("Append efficiency: Multiple appends should use constant bandwidth", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			err = alice.StoreFile(aliceFile, []byte(contentOne))
+			Expect(err).To(BeNil())
+
+			// Measure bandwidth for first append
+			bw1 := userlib.DatastoreGetBandwidth()
+			err = alice.AppendToFile(aliceFile, []byte("X"))
+			Expect(err).To(BeNil())
+			bw1 = userlib.DatastoreGetBandwidth() - bw1
+
+			// Append many more times
+			for i := 0; i < 100; i++ {
+				err = alice.AppendToFile(aliceFile, []byte("X"))
+				Expect(err).To(BeNil())
+			}
+
+			// Measure bandwidth for 102nd append
+			bw2 := userlib.DatastoreGetBandwidth()
+			err = alice.AppendToFile(aliceFile, []byte("X"))
+			Expect(err).To(BeNil())
+			bw2 = userlib.DatastoreGetBandwidth() - bw2
+
+			// Bandwidth should be approximately the same
+			Expect(bw2).To(BeNumerically("~", bw1, float64(bw1)*0.1))
+		})
+
+		Specify("Append efficiency: Should not scale with file size", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			// Create small file and append
+			err = alice.StoreFile(aliceFile, []byte("small"))
+			Expect(err).To(BeNil())
+
+			bw1 := userlib.DatastoreGetBandwidth()
+			err = alice.AppendToFile(aliceFile, []byte("X"))
+			Expect(err).To(BeNil())
+			bw1 = userlib.DatastoreGetBandwidth() - bw1
+
+			// Create large file (1MB) and append
+			largeContent := make([]byte, 1000000)
+			err = alice.StoreFile(bobFile, largeContent)
+			Expect(err).To(BeNil())
+
+			bw2 := userlib.DatastoreGetBandwidth()
+			err = alice.AppendToFile(bobFile, []byte("X"))
+			Expect(err).To(BeNil())
+			bw2 = userlib.DatastoreGetBandwidth() - bw2
+
+			// Bandwidth should be similar regardless of file size
+			Expect(bw2).To(BeNumerically("~", bw1, float64(bw1)*0.1))
+		})
+
+		Specify("Append efficiency: Empty append should use minimal bandwidth", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			err = alice.StoreFile(aliceFile, []byte(contentOne))
+			Expect(err).To(BeNil())
+
+			bw := userlib.DatastoreGetBandwidth()
+			err = alice.AppendToFile(aliceFile, []byte(""))
+			Expect(err).To(BeNil())
+			bw = userlib.DatastoreGetBandwidth() - bw
+
+			// Should use minimal bandwidth (just overhead, no actual content)
+			Expect(bw).To(BeNumerically("<", 5000))
+		})
+
+		Specify("Append efficiency: Should not scale with previous append size", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			err = alice.StoreFile(aliceFile, []byte("initial"))
+			Expect(err).To(BeNil())
+
+			// Append huge amount of data
+			hugeContent := make([]byte, 1000000)
+			err = alice.AppendToFile(aliceFile, hugeContent)
+			Expect(err).To(BeNil())
+
+			// Next small append should not download the huge content
+			bw := userlib.DatastoreGetBandwidth()
+			err = alice.AppendToFile(aliceFile, []byte("small"))
+			Expect(err).To(BeNil())
+			bw = userlib.DatastoreGetBandwidth() - bw
+
+			// Should be small, not proportional to 1MB
+			Expect(bw).To(BeNumerically("<", 10000))
+		})
+
+		Specify("Append efficiency: Should not scale with number of shares", func() {
+			alice, err = client.InitUser("alice", defaultPassword)
+			Expect(err).To(BeNil())
+
+			// Create and share with multiple users
+			users := make([]*client.User, 10)
+			for i := 0; i < 10; i++ {
+				users[i], err = client.InitUser(fmt.Sprintf("user%d", i), defaultPassword)
+				Expect(err).To(BeNil())
+			}
+
+			err = alice.StoreFile(aliceFile, []byte(contentOne))
+			Expect(err).To(BeNil())
+
+			for i := 0; i < 10; i++ {
+				invite, err := alice.CreateInvitation(aliceFile, fmt.Sprintf("user%d", i))
+				Expect(err).To(BeNil())
+				err = users[i].AcceptInvitation("alice", invite, fmt.Sprintf("file%d", i))
+				Expect(err).To(BeNil())
+			}
+
+			// Append bandwidth should not depend on number of shares
+			bw := userlib.DatastoreGetBandwidth()
+			err = alice.AppendToFile(aliceFile, []byte("X"))
+			Expect(err).To(BeNil())
+			bw = userlib.DatastoreGetBandwidth() - bw
+
+			Expect(bw).To(BeNumerically("<", 10000))
+		})
 	})
 })
